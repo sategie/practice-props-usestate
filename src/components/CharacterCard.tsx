7aae0cbd2f86510b2dbe83c109a13e513c603a2d
sategie <sategie@yahoo.com>
2024-09-25 22:49:03 +0200

    Set up components.

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+
+
+export default function CharacterCard() {
+
+
+
+    return(
+        <>
+            <h1>This will contain the information for a single Rick&Morty character.</h1>
+        </>
+    )
+}
---
+++ src/components/CharacterList.tsx
@@ -0,0 +1,11 @@
+
+
+export default function CharacterList() {
+
+
+    return(
+        <>
+            <h1>This will contain the list of all the characters in the Rick&Morty API.</h1>
+        </>
+    )
+}
